08b18b6b4a39ee48e3268f62d33d45127c171157
Ochuodho <ochuodhoderrick@gmail.com>
2023-09-19 18:33:44 +0300

    Completed assignment

---
+++ attandmethods.py
@@ -0,0 +1,34 @@
+class Album:
+    album_count = 0
+
+    def __init__(self, date):
+        self.increase_album_count()
+        
+        self.release_date = date
+
+    @classmethod
+
+    def increase_album_count(cls, increment=1):
+        cls.album_count += increment
+
+pass
+
+class Album:
+    GENRES = ["Hip-Hop", "Pop", "Jazz"]
+    album_count = 0
+
+    def __init__(self, genre, date):
+        if self.check_genre(genre):
+            self.increase_album_count()
+            self.genre = genre
+            self.release_date = date
+
+    @classmethod
+    def check_genre(cls, genre):
+        return genre in cls.GENRES
+
+    @classmethod
+    def increase_album_count(cls, increment=1):
+        cls.album_count += increment
+        
+
